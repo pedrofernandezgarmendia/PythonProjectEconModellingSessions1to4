df4ba46716bfa2b599d0ec41cfa2396b7ef28282
pedrofernandez <pedrofernandezgarmendia@gmail.com>
2025-03-26 18:07:25 +0100

    Session 3 and 4

---
+++ pokerwed.py
@@ -0,0 +1,85 @@
+from deck import Deck, Card
+
+class PokerHand:
+    def __init__(self, deck):
+
+        cards = []
+        for i in range(5):
+            cards.append(deck.deal())
+        self._cards = cards
+
+    @property
+    def cards(self):
+        return self._cards
+
+    def __str__(self):
+        return str(self.cards)
+
+    @property
+    def is_flush(self):
+        for card in self.cards[1:]:
+            if self.cards[0].suit != card.suit:
+                return False
+        return True
+
+    matches = 0
+
+    @property
+    def is_full_house(self):
+        return self.number_matches == 8
+
+
+    @property
+    def number_matches(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+            if self.cards[i].rank == self.cards[j].rank:
+                matches += 1
+
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.number_matches == 2: # simple way
+            return True
+        return False
+
+    @property
+    def is_two_pair(self):
+        if self.number_matches == 4: # do it this way, the more advanced doesn't work
+            return True
+        return False
+
+    @property
+    def is_quad_pair(self):
+        if self.number_matches == 12:
+            return True
+        return False
+
+    @property
+    def is_straight(self):
+        self.cards.sort()
+        distance = Card.RANKS.index(self.cards[4].rank) - \
+                   Card.RANKS.index(self.cards[0].rank)
+        return self.number_matches == 0 and distance == 4
+
+
+count = 0
+matches = 0             # reuse the code for matchs like I did below
+while matches < 1000:
+    deck = Deck()
+    deck.shuffle()
+    hand = PokerHand(deck)
+    if hand.is_pair:
+        matches += 1
+        print(hand)
+    count += 1
+    if hand.is_full_house:
+        matches += 1
+        print(hand)
+    count += 1
+
+print(f"probability of a full house is {100*matches/count}%")
